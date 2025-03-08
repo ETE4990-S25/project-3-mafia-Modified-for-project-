98e87c229c3a4408bbae5d413088d60445f1fd46
Brianna Argueta <briannaarguetamusic@gmail.com>
2025-03-07 22:03:01 -0800

    the intro

---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+echo "# project-1-mafia" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ETE4990-S25/project-1-mafia.git
+git push -u origin main
+
+input ("Press Enter to start:")
+print ('Welcome, I am your narrator... 
+       I dont often see many new people around this town..
+         Lets find out who you are '):
+
+         
