697f2fad66b25afae2bf3b27aab5372865a48e91
Brianna Argueta <briannaarguetamusic@gmail.com>
2025-03-07 22:48:10 -0800

    try

---
+++ Mafia intro in py file.py
@@ -0,0 +1,22 @@
+# Mafia intro in py file
+
+input( "press enter to start:")
+print ( "wire sounds ")
+print (" flashing lights")
+print ( " woow you are spining out of control" )
+print ( " try to stop the spining")
+input (" press your enter to stop")
+
+print ( " it seems like you have entered a time machine " )
+print ( " Welcome, or should I say good luck..." )
+
+input("press enter to continue:")
+print(" my name is Fern. I am the all seeing eye... I will be the narrator of this story")
+print ("I dont often see many new people around this town...Lets find out who you are")
+choice = input ( "Press 1 for Murder or 2 for Detective")
+if choice == '1':
+  print ("ooo scary a murder is in town")
+elif choice == '2':
+  print (" so theres a new sherif in town then...")
+else:
+  print("Pick again 1 or 2.")
