933204306b433e4f2bdb556a7689570db1d28689
Alex Reyes <alexre2801@gmail.com>
2025-03-12 22:43:28 -0700

    initial upload, file for creation/handling of items

---
+++ mafia_items.py
@@ -0,0 +1,5 @@
+class basicItems(object):
+    """Establishes the framework for items found in player inventory"""
+    def __init__(self,name,description):
+        self.Itemname=name
+        self.ItemDesc=description
