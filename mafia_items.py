6b07972cf50b764f332f0399615dc5da4e49d5cc
Alex Reyes <alexre2801@gmail.com>
2025-03-16 16:35:35 -0700

    Finished the basic class for setting up items, created "weapons" for murderer as a test

--- mafia_items.py
+++ mafia_items.py
@@ -1,5 +1,17 @@
 class basicItems(object):
     """Establishes the framework for items found in player inventory"""
-    def __init__(self,name,description):
+    def __init__(self,name,description,uses):
         self.Itemname=name
         self.ItemDesc=description
+        self.itemUses=uses
+
+
+#creating the murderer items, could be placed in main file but going here for now for testing 
+#Addendum: decided to create it in the class file instead
+#"Pillow","Gun","Knife"
+#Knife=basicItems("Knife","BLABLABLA THIS IS A TEST DESCRIPTION",2)
+#Gun=basicItems("Gun","BLABLABLA THIS IS A TEST DESCRIPTION",3)
+#Pillow=basicItems("Pillow","BLABLABLA THIS IS A TEST DESCRIPTION",1)
+
+
+
